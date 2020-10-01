1c00ccd9a5e1d362533b66a4a9513604e85be4d6
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-01 04:21:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,11 +10,11 @@ def add_element_to_end_of_array(array, element)
   create_an_array.push("arrays!")
 end
 
-def add_element_to_start_of_array(create_an_array, "wow")
+def add_element_to_start_of_array(array, element)
   create_an_array.unshift("wow")
 end
 
-def remove_element_from_end_of_array(create_an_array)
+def remove_element_from_end_of_array(array)
   create_an_array.pop
 end
 
