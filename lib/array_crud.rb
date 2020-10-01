ae488fae05c13d44a34b9a9b6ba8bcf0fb9e3c61
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-01 04:37:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ def remove_element_from_end_of_array(create_an_array)
   create_an_array.pop
 end
 
-def remove_element_from_start_of_array(array)
+def remove_element_from_start_of_array(create_an_array)
   create_an_array.shift
 end
 
